718340d19cc2cb45aedea58d391c7c5a25570cb0
AroLeaf <starleafofa@gmail.com>
2022-03-29 11:09:31 +0200

    [fix] pairs without closing token caused an infinite loop

--- parser.js
+++ parser.js
@@ -15,8 +15,8 @@ export default class Parser {
   }
 
   wraps(pred, cb, matchEnd = false) {
-    return this.find(pred, index => cb(this.tokens.slice(this.pos + 1, index)) || true)
-      || (matchEnd && cb(this.tokens.slice(this.pos + 1)) || true);
+    return this.find(pred, index => cb(this.tokens.slice(this.pos + 1, index)) ?? true)
+      || ((matchEnd && cb(this.tokens.slice(this.pos + 1))) ?? true);
   }
 
   push(node) {
@@ -65,7 +65,7 @@ export default class Parser {
 
         // unordered list
         case types.list.unordered: {
-          this.wraps(t =>
+          if (this.wraps(t =>
             (t.type === types.whitespace && t.value > 1)
             || (t.type === types.list.unordered && t.value !== token.value)
             || t.type === types.list.ordered
@@ -76,12 +76,15 @@ export default class Parser {
             const items = splitArray(children, t => t.type === types.list.unordered);
             this.push(new nodes.unorderedList(items.map(i => new Parser(i, { type: 'inline' }).parse())));
             this.pos += children.length + 1;
-          }, true); break;
+          }, true)) break;
+
+          this.push(new nodes.text(token.raw));
+          this.pos++; break;
         }
 
         // ordered list
         case types.list.ordered: {
-          this.wraps(t =>
+          if (this.wraps(t =>
             (t.type === types.whitespace && t.value > 1)
             || t.type === types.list.unordered
             || t.type === types.title
@@ -91,11 +94,14 @@ export default class Parser {
             const items = splitArray(children, t => t.type === types.list.ordered);
             this.push(new nodes.orderedList(items.map(i => new Parser(i, { type: 'inline' }).parse())));
             this.pos += children.length + 1;
-          }, true); break;
+          }, true)) break;
+
+          this.push(new nodes.text(token.raw));
+          this.pos++; break;
         }
 
         case types.quote: {
-          this.wraps(t => 
+          if (this.wraps(t => 
             (t.type === types.whitespace && t.value > 1)
             || t.type === types.list.unordered
             || t.type === types.list.ordered  
@@ -105,18 +111,22 @@ export default class Parser {
             const items = splitArray(children, t => t.type === types.quote);
             this.push(new nodes.quote(items.map(i => new Parser(i, { type: 'inline' }).parse())));
             this.pos += children.length + 1;
-          }, true); break;
+          }, true)) break;
+
+          this.push(new nodes.text(token.raw));
+          this.pos++; break;
         }
 
         // title
         case types.title: {
-          this.wraps(t => t.type === types.whitespace && t.value > 0, children => {
+          if (this.wraps(t => t.type === types.whitespace && t.value > 0, children => {
             this.push(new nodes.title(new Parser(children, {
               type: 'inline',
             }).parse(), token.value));
             this.pos += children.length + 1;
-          }, true); 
-          this.paragraph(); break;
+          }, true)) { this.paragraph(); break; }
+          this.push(new nodes.text(token.raw));
+          this.pos++; break;
         }
 
         // link
@@ -167,42 +177,54 @@ export default class Parser {
         
         // bold
         case types.bold: {
-          this.wraps(t => t.type === types.bold, children => {
+          if (this.wraps(t => t.type === types.bold, children => {
             this.push(new Parser(children, {
               type: nodes.bold,
             }).parse());
             this.pos += children.length + 2;
-          }); break;
+          })) break;
+
+          this.push(new nodes.text(token.raw));
+          this.pos++; break;
         }
 
         // underline
         case types.underline: {
-          this.wraps(t => t.type === types.underline, children => {
+          if (this.wraps(t => t.type === types.underline, children => {
             this.push(new Parser(children, {
               type: nodes.underline,
             }).parse());
             this.pos += children.length + 2;
-          }); break;
+          })) break;
+
+          this.push(new nodes.text(token.raw));
+          this.pos++; break;
         }
 
         // italics
         case types.italics: {
-          this.wraps(t => t.type === types.italics && t.value === token.value, children => {
+          if (this.wraps(t => t.type === types.italics && t.value === token.value, children => {
             this.push(new Parser(children, {
               type: nodes.italics,
             }).parse());
             this.pos += children.length + 2;
-          }); break;
+          })) break;
+
+          this.push(new nodes.text(token.raw));
+          this.pos++; break;
         }
 
         // strikethrough
         case types.strikethrough: {
-          this.wraps(t => t.type === types.strikethrough, children => {
+          if (this.wraps(t => t.type === types.strikethrough, children => {
             this.push(new Parser(children, {
               type: nodes.strikethrough,
             }).parse());
             this.pos += children.length + 2;
-          }); break;
+          })) break;
+          
+          this.push(new nodes.text(token.raw));
+          this.pos++; break;
         }
         
         // whitespace
@@ -222,8 +244,8 @@ export default class Parser {
       }
     }
 
-    if (this.nodes.at(0) instanceof nodes.whitespace) this.nodes.splice(0, 1);
-    if (this.nodes.at(-1) instanceof nodes.whitespace) this.nodes.splice(this.nodes.length - 1, 1);
+    if (this.nodes.at(0) instanceof nodes.whitespace && this.nodes.length > 2) this.nodes.splice(0, 1);
+    if (this.nodes.at(-1) instanceof nodes.whitespace && this.nodes.length > 2) this.nodes.splice(-1, 1);
 
     return this.type === 'inline'
       ? this.nodes
